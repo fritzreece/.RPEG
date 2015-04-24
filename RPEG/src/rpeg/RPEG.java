6645424500b5c5a9213e248879bfc4e72ff20df8
csstudent <csstudent@128.135.91.78>
2015-04-24 14:15:52 -0500

    We can finally upload?

--- RPEG/src/rpeg/RPEG.java
+++ RPEG/src/rpeg/RPEG.java
@@ -5,9 +5,12 @@
  */
 package rpeg;
 
+import java.io.IOException;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
@@ -20,26 +23,18 @@ import javafx.stage.Stage;
 public class RPEG extends Application {
     
     @Override
-    public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say 'Burn The Whales'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-            
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Burn the Whales!");
-            }
-        });
+    public void start(Stage primaryStage) throws IOException {
         
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-        
-        Scene scene = new Scene(root, 300, 250);
-        
-        primaryStage.setTitle("Hello World!");
+   
+       Parent root = FXMLLoader.load(getClass().getResource("UnfinishedMap.fxml"));
+    
+        Scene scene = new Scene(root, 300, 275);
+    
+        primaryStage.setTitle("FXML Welcome");
         primaryStage.setScene(scene);
         primaryStage.show();
     }
+    
 
     /**
      * @param args the command line arguments
