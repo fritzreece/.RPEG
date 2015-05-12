c612b3216ec3f74c3b0e562fc16dd5b58b655e76
tginsbu <tginsbu@aol.com>
2015-05-12 14:36:03 -0500

    I will beat git if its the last thing I do.

--- RPEG/src/BackEnd/WorldMap.java
+++ RPEG/src/BackEnd/WorldMap.java
@@ -4,7 +4,21 @@
  * and open the template in the editor.
  */
 package BackEnd;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.FontMetrics;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.Rectangle;
+import java.awt.Shape;
+import java.awt.image.BufferedImage;
+import java.awt.image.ImageObserver;
 import java.io.File;
+import java.io.IOException;
+import java.text.AttributedCharacterIterator;
+import javax.imageio.ImageIO;
+import javax.swing.JPanel;
 /**
  *
  * @author Fritz Reece
@@ -18,27 +32,44 @@ public class WorldMap {
         width = dim1;
         height = dim2;
         background = bg;
-        tiles = new MapTile[dim1][dim2];
+        tiles = new MapTile[dim1/40][dim2/40];
         for(int j=0;j<tiles.length;j++){
             MapTile[] mt = tiles[j];
             for(int i=0;i<mt.length;i++){
-                mt[i] = new MapTile(i,j,TileType.GRASS,this);
+                mt[i] = new MapTile(i*40,j*40,TileType.GRASS, this);
             }
         }
     }
     //returns true if it succeeds
-    public boolean addThing(MapThing thing, int x, int y){
-        return tiles[y][x].add(thing);
-    }
+   // public boolean addThing(MapThing thing, int x, int y){
+     //   return tiles[y][x].add(thing);
+  //  }
     public MapTile getTile(int x, int y){
         return tiles[y][x];
     }
-    
-   public int getWidth() {
-        return width;
+    public void createMap() throws IOException {
+        BufferedImage bi = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+        
+        Graphics2D ig2 = bi.createGraphics();
+        for(int i = 0; i <= width/40; i++) {
+            for(int j = 0; j <=height/40; j++) {
+                if(tiles[i][j].getTileType() == TileType.GRASS) {
+                    BufferedImage img = null;
+                    JPanel test = new JPanel();
+                    try {
+                     img = ImageIO.read(new File("../rpet/Textures/grassTile1.jpg"));
+                     ig2.drawImage(img, i*40, j*40, test);
+                        } catch (IOException e) {
+                        }
+                   // ig2.drawImage("");
+                    
+                }
+                }
+            }
+        ImageIO.write(bi, "PNG", new File("../rpeg/Textures/map.png"));
+        }
+        
+
         
     }
-    public int getHeight() {
-        return height;
-    }
-}
+
